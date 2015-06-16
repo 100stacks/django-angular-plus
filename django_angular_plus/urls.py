14d6555073dea182bdb46f697bb684d36de66f56
site-demo <site9demo@monumentmail.com>
2015-06-15 23:47:14 -0500

    Added API endpoint for AccountViewSet

--- django_angular_plus/urls.py
+++ django_angular_plus/urls.py
@@ -1,9 +1,22 @@
+from rest_framework_nested import routers
+
+from authentication.views import AccountViewSet
+
+'''
+default generated imports...
+
 from django.conf.urls import patterns, url
 
 from django_angular_plus.views import IndexView
+'''
+
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
 
 urlpatterns = patterns(
     '',
+
+    url(r'^api/v1/', include(router.urls)),
 
     url('^.*$', IndexView.as_view(), name='index'),
 )
